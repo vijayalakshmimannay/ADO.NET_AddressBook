ce36bfa1d33cdc0dba4d56b33529ce5186fde57f
vijayalakshmimannay <vijayalakshmimannay@gmail.com>
2022-06-29 19:35:40 +0530

    [vijayalakshmi] add multi addressbook using multithreading

--- ADO.NET_AddressBook/AddressBookSystem/AddressBook.cs
+++ ADO.NET_AddressBook/AddressBookSystem/AddressBook.cs
@@ -11,7 +11,7 @@ namespace AddressBookSystem
         SqlConnection connect = new SqlConnection(dbpath);
 
         //retrieve data from table
-        /*public void GetAddressbook()
+        public void GetAddressbook()
         {
             AddressBookSystem.ModelClass model = new AddressBookSystem.ModelClass();
             SqlConnection connect = new SqlConnection(dbpath);
@@ -27,16 +27,16 @@ namespace AddressBookSystem
                     while (reader.Read())
                     {
                         model.ID = reader.GetInt32(0);
-                        model.NAME = reader.GetString(1);
+                        model.FIRSTNAME = reader.GetString(1);
                         model.LASTNAME = reader.GetString(2);
-                        model.ADDRESS = reader.GetString(3);
+                        model.ADDRESSs = reader.GetString(3);
                         model.CITY = reader.GetString(4);
                         model.STATE = reader.GetString(5);
                         model.ZIP = reader.GetString(6);
                         model.PHONENO = reader.GetString(7);
                         model.EMAIL = reader.GetString(8);
-                        Console.WriteLine("ID: " + model.ID + "\nNAME: " + model.NAME + "\nLastName: " + model.LASTNAME +
-                               "\nAddress" + model.ADDRESS + "\nCity: " + model.CITY + "\nState:" + model.STATE + "\nZipCode: " + model.ZIP
+                        Console.WriteLine("ID: " + model.ID + "\nNAME: " + model.FIRSTNAME + "\nLastName: " + model.LASTNAME +
+                               "\nAddress" + model.ADDRESSs + "\nCity: " + model.CITY + "\nState:" + model.STATE + "\nZipCode: " + model.ZIP
                                + "\nPhone: " + model.PHONENO + "\nEmail: " + model.EMAIL + "\n");
                     }
                 }
@@ -47,46 +47,32 @@ namespace AddressBookSystem
                 reader.Close();
             }
             connect.Close();
-        }*/
+        }
 
-        public void CreateNewContact()
+     
+        public void CreateNewContact(ModelClass model)
         {
             SqlConnection connect = new SqlConnection(dbpath);
-            using (connect)
-            {
-                connect.Open();
-                AddressBookSystem.ModelClass model = new AddressBookSystem.ModelClass();
-                Console.WriteLine("Enter First Name");
-                model.FIRSTNAME = Console.ReadLine();
-                Console.WriteLine("Enter Last Name");
-                model.LASTNAME = Console.ReadLine();
-                Console.WriteLine("Enter Address ");
-                model.ADDRESSs = Console.ReadLine();
-                Console.WriteLine("Enter City ");
-                model.CITY = Console.ReadLine();
-                Console.WriteLine("Enter State ");
-                model.STATE = Console.ReadLine();
-                Console.WriteLine("Enter Zip Code ");
-                model.ZIP = Console.ReadLine();
-                Console.WriteLine("Enter Phone ");
-                model.PHONENO = Console.ReadLine();
-                Console.WriteLine("Enter Email ");
-                model.EMAIL = Console.ReadLine();
-                SqlCommand sql = new SqlCommand("SPAddress_Book", connect);
-                sql.CommandType = CommandType.StoredProcedure;
-                sql.Parameters.AddWithValue("@FIRSTNAME", model.FIRSTNAME);
-                sql.Parameters.AddWithValue("@LASTNAME", model.LASTNAME);
-                sql.Parameters.AddWithValue("@ADDRESSs", model.ADDRESSs);
-                sql.Parameters.AddWithValue("@CITY", model.CITY);
-                sql.Parameters.AddWithValue("@STATE", model.STATE);
-                sql.Parameters.AddWithValue("@ZIP", model.ZIP);
-                sql.Parameters.AddWithValue("@PHONENO", model.PHONENO);
-                sql.Parameters.AddWithValue("@EMAIL", model.EMAIL);
-                sql.ExecuteNonQuery();
-                Console.WriteLine("Record created successfully.");
-                connect.Close();
-            }
+            lock (this)
+                using (connect)
+                {
+                    connect.Open();
+                    SqlCommand sql = new SqlCommand("SPAddress_Book", connect);
+                    sql.CommandType = CommandType.StoredProcedure;
+                    sql.Parameters.AddWithValue("@FIRSTNAME", model.FIRSTNAME);
+                    sql.Parameters.AddWithValue("@LASTNAME", model.LASTNAME);
+                    sql.Parameters.AddWithValue("@ADDRESSs", model.ADDRESSs);
+                    sql.Parameters.AddWithValue("@CITY", model.CITY);
+                    sql.Parameters.AddWithValue("@STATE", model.STATE);
+                    sql.Parameters.AddWithValue("@ZIP", model.ZIP);
+                    sql.Parameters.AddWithValue("@PHONENO", model.PHONENO);
+                    sql.Parameters.AddWithValue("@EMAIL", model.EMAIL);
+                    sql.ExecuteNonQuery();
+                    Console.WriteLine("Record created successfully.");
+                    connect.Close();
+                }
         }
+
 
         //Update table record
         public void UpdateRecord()
@@ -142,5 +128,21 @@ namespace AddressBookSystem
 
             }
         }
+        public void AddMultipleEmployee(List<ModelClass> data)
+        {
+            data.ForEach(details =>
+            {
+                Thread thread = new Thread(() =>
+                {
+                    Console.WriteLine("Thread Start Time: " + DateTime.Now);
+                    this.CreateNewContact(details);
+                    Console.WriteLine("Contact Added: " + details.FIRSTNAME);
+                    Console.WriteLine("Thread End Time: " + DateTime.Now);
+                });
+                thread.Start();
+            });
+        }
     }
 }
+
+
--- ADO.NET_AddressBook/Program.cs
+++ ADO.NET_AddressBook/Program.cs
@@ -10,8 +10,35 @@ namespace AddressBookSystem
             //contact.GetAddressbook();
             //contact.CreateNewContact();
             //contact.UpdateRecord();
-            contact.DeleteRecord();
+            //contact.DeleteRecord();
 
+            List<ModelClass> list = new List<ModelClass>();
+            Console.WriteLine("Enter number of contacts to Add");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int i = 0;
+            while (i < number)
+            {
+                AddressBookSystem.ModelClass model = new AddressBookSystem.ModelClass();
+                Console.WriteLine("Enter First Name");
+                model.FIRSTNAME = Console.ReadLine();
+                Console.WriteLine("Enter Last Name");
+                model.LASTNAME = Console.ReadLine();
+                Console.WriteLine("Enter Address ");
+                model.ADDRESSs = Console.ReadLine();
+                Console.WriteLine("Enter City ");
+                model.CITY = Console.ReadLine();
+                Console.WriteLine("Enter State ");
+                model.STATE = Console.ReadLine();
+                Console.WriteLine("Enter Zip Code ");
+                model.ZIP = Console.ReadLine();
+                Console.WriteLine("Enter Phone ");
+                model.PHONENO = Console.ReadLine();
+                Console.WriteLine("Enter Email ");
+                model.EMAIL = Console.ReadLine();
+                list.Add(model);
+                i++;
+            }
+            contact.AddMultipleEmployee(list);
         }
     }
 }
